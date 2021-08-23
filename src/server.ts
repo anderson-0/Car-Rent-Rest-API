bc3565b805bb7f52c2284f78fbc6a4661cb596b0
AndersonDaSilvaOSF <43174799+AndersonDaSilvaOSF@users.noreply.github.com>
2021-08-22 21:49:23 -0400

    feature: adding to swagger url to app

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
 import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.get('/', (_, response) => {
