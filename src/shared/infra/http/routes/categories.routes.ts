76674ce7d71d8937b69e16d6aae9c9abda875a98
Anderson <anderson.consultant.it@outlook.com>
2021-08-29 18:16:11 -0400

    feature: required authentication and admin role for certain routes

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,9 @@ import { ImportCategoryController } from "@modules/cars/useCases/importCategory/
 import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticate";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+
 const categoriesRoutes = Router();
 
 const upload = multer({
@@ -14,9 +17,13 @@ const upload = multer({
 const importCategoryController = new ImportCategoryController();
 const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
-categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
+
+categoriesRoutes.use(ensureAuthenticated);
+categoriesRoutes.use(ensureAdmin);
+
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.post(
   "/import",
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
+
 import { ListSpecificationsController } from "@modules/cars/useCases/listSpecifications/ListSpecificationsController";
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticate";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
-specificationsRoutes.post("/", createSpecificationController.handle);
-
 specificationsRoutes.get("/", listSpecificationsController.handle);
+
+specificationsRoutes.use(ensureAuthenticated);
+specificationsRoutes.use(ensureAdmin);
+
+specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
