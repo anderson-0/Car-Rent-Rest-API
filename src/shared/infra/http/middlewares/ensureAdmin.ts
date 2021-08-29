f618f9b144d9c56ecf05750796ea9fb738d2b6c8
Anderson <anderson.consultant.it@outlook.com>
2021-08-29 17:52:34 -0400

    feature: ensureAdmin middleware

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { AppError } from "@shared/errors/AppError";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { id } = req.user;
+
+  const usersRepository = new UsersRepository();
+  const user = await usersRepository.findById(id);
+
+  if (!user.isAdmin) throw new AppError("User is not admin");
+  next();
+}
