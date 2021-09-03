8f1c5a25f23aab7153fb8582373ca2bd9f44f889
Anderson <anderson.consultant.it@outlook.com>
2021-09-03 14:34:24 -0400

    feature: List Rentals by user use case

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,16 +2,20 @@ import { Router } from "express";
 
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticate";
+
 import { CreateRentalController } from "@modules/rentals/useCases/createRentalUseCase/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
-rentalsRoutes.use(ensureAuthenticated);
-rentalsRoutes.use(ensureAdmin);
+const listRentalsByUserController = new ListRentalsByUserController();
 
+rentalsRoutes.use(ensureAuthenticated);
+
+rentalsRoutes.get("/", listRentalsByUserController.handle);
 rentalsRoutes.post("/", createRentalController.handle);
 rentalsRoutes.post("/devolution/:id", devolutionRentalController.handle);
 
