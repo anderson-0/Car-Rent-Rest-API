58fb11d67cd6b9e572e47a4842587e9592dbbc85
Anderson <anderson.consultant.it@outlook.com>
2021-09-04 19:33:08 -0400

    refactor: ensureAuthenticate middleware expects refreshToken

--- src/shared/infra/http/middlewares/ensureAuthenticate.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticate.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import { AppError } from "@shared/errors/AppError";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import auth from "@config/auth";
 
 interface IPayload {
   sub: string;
@@ -13,15 +15,18 @@ export async function ensureAuthenticated(
   next: NextFunction
 ) {
   const authHeader = req.headers.authorization;
+  const userTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) throw new AppError("Token missing", 401);
 
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: userId } = verify(token, "secret") as IPayload;
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(userId);
+    const { sub: userId } = verify(token, auth.secretRefreshToken) as IPayload;
+    const user = await userTokensRepository.findByUserIdAndRefreshToken(
+      userId,
+      token
+    );
 
     if (!user) throw new AppError("User does not exists", 401);
 
