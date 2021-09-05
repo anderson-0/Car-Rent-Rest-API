a6ddc4cf50a3d024bce6953c058ec8f74778cf02
Anderson <anderson.consultant.it@outlook.com>
2021-09-05 16:01:07 -0400

    feature: user avatar and car images use cases now using storage provider

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,18 +1,21 @@
-import { deleteFile } from "@utils/file";
 import { inject, injectable } from "tsyringe";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUpdateUserAvatarDTO } from "@modules/accounts/dtos/IUpdateUserAvatarDTO";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 @injectable()
 class UpdateUserAvatarUseCase {
   constructor(
-    @inject("UsersRepository") private usersRepository: IUsersRepository
+    @inject("UsersRepository") private usersRepository: IUsersRepository,
+    @inject("StorageProvider") private storageProvider: IStorageProvider
   ) {}
   async execute({ userId, avatarFile }: IUpdateUserAvatarDTO): Promise<void> {
     const user = await this.usersRepository.findById(userId);
 
-    if (user.avatar) await deleteFile(`./tmp/avatar/${user.avatar}`);
+    if (user.avatar) await this.storageProvider.delete(user.avatar, "avatar");
+
+    await this.storageProvider.save(avatarFile, "avatar");
 
     user.avatar = avatarFile;
 
--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -2,15 +2,18 @@ import { inject, injectable } from "tsyringe";
 
 import { IUploadCarsImageDTO } from "@modules/cars/dtos/IUploadCarsImageDTO";
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 @injectable()
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImagesRepository
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider") private storageProvider: IStorageProvider
   ) {}
   async execute({ car_id, images_name }: IUploadCarsImageDTO): Promise<void> {
     images_name.map(async image => {
+      await this.storageProvider.save(image, "cars");
       await this.carsImagesRepository.create(car_id, image);
     });
   }
