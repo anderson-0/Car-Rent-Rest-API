74d00cce847aa5dc51b0ab5ddc64c670d29ae187
Anderson <anderson.consultant.it@outlook.com>
2021-09-06 18:40:17 -0400

    fix: database connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,15 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (): Promise<Connection> => {
+export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-
-  const isTestEnv = process.env.NODE_ENV === "test";
 
   return createConnection(
     Object.assign(defaultOptions, {
-      database: isTestEnv ? "rentx_test" : defaultOptions.database,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentx_test"
+          : defaultOptions.database,
     })
   );
 };
