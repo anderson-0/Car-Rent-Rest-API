ccdae63a57ee5a9d73cb7fc17b59fb2b3a778d31
Anderson <anderson.consultant.it@outlook.com>
2021-09-06 20:50:37 -0400

    feature: included sentry for better error management

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import "@shared/container";
 import upload from "@config/upload";
@@ -15,8 +17,26 @@ import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
 const app = express();
-
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -30,6 +50,8 @@ app.use(router);
 app.get("/", (_, response) => {
   return response.json({ ok: true });
 });
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
